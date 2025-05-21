e37b890f3052c51c28b08affbc4db643298a3a95
darweshiamerson <darweshiamerson@gmail.com>
2025-05-21 02:47:04 -0500

    Began working on adding card to frontend.

--- Front/card.cpp
+++ Front/card.cpp
@@ -4,13 +4,14 @@
 using namespace std;
 
 class Card {
-    private:
+protected:
+        // Card ID
         int id;
         string name;
         int power;
         int cost;
 
-    public:
+public:
         Card(int id, string name, int power, int cost)
             : id(id), name(name), power(power), cost(cost) {
             if (power < 0) {
@@ -19,9 +20,12 @@ class Card {
             }
         }
 
-        void onRevealEffect() {
-
+        virtual void onRevealEffect() {
+            cout << name << "Has no effect." << endl;
         }
+        
+        //Used for JSON serialization with react
+        virtual string toJSON() const;
 
         int getId() const {
             return id;
@@ -37,5 +41,25 @@ class Card {
 
         int getCost() const {
             return cost;
+        }
+
+        void printInfo() const {
+            cout << "Card ID: " << id << endl;
+            cout << "Name: " << name << endl;
+            cout << "Power: " << power << endl;
+            cout << "Cost: " << cost << endl;
+        }
+};
+
+class superman : public Card{
+public:
+        superman() : Card(1, "superman", 6, 4) {}
+
+        void onRevealEffect() override {
+            cout << "I am Earth's Protector" << endl;
+        }
+
+        string toJSON() const override {
+            return "{ \"id\":1,\"name\":\"superman\",\"power\":6,\"cost\":4}"; 
         }
 };
